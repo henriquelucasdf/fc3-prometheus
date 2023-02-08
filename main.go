fbaf386d52e6334c17b97136e5737ab35dfc6cab
henriquelucasdf <henriquelucasdf@gmail.com>
2023-02-07 23:33:03 -0300

    add count and histogram metrics

--- main.go
+++ main.go
@@ -16,9 +16,21 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_http_requests_total",
+	Help: "Count of all http requests for goapp",
+}, []string{})
+
+var httpDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "goapp_http_request_duration",
+	Help: "Duration in seconds of all http requests",
+}, []string{"handler"})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
+	r.MustRegister(httpRequestsTotal)
+	r.MustRegister(httpDuration)
 
 	// random assignement for metric
 	go func() {
@@ -26,6 +38,20 @@ func main() {
 			onlineUsers.Set(float64(rand.Intn(2000)))
 		}
 	}()
+
+	// create home page
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello Full Cycle"))
+	})
+
+	// duration
+	d := promhttp.InstrumentHandlerDuration(
+		httpDuration.MustCurryWith(prometheus.Labels{"handler": "home"}),
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, home),
+	)
+
+	http.Handle("/", d)
 
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
